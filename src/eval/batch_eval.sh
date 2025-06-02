0442566c182375e48ffe15d3717f4621d65a8725
alejogrillor02 <alejo.grillor02@protonmail.com>
2025-06-02 15:13:56 -0400

    Fix batch_eval.sh

--- src/eval/batch_eval.sh
+++ src/eval/batch_eval.sh
@@ -13,6 +13,7 @@ if [[ ! -f "${PROJECTDIR}/config.yaml" ]]; then
 	echo "Error: config.yaml not found in project root!" >&2
 	exit 1
 fi
+export PROJECTDIR
 
 OUTPUT_DIR=${PROJECTDIR}/$(yq -r '.OUTPUT_DIR' ${PROJECTDIR}/config.yaml)
 MODEL_DIR=${PROJECTDIR}/$(yq -r '.MODEL_DIR' ${PROJECTDIR}/config.yaml)

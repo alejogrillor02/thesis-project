f4f656e65700f573551819dfba8df4c9e15fbc9f
alejogrillor02 <alejo.grillor02@protonmail.com>
2025-06-15 04:41:49 -0400

    TEST

--- src/tests/grad_shap.py
+++ src/tests/grad_shap.py
@@ -9,7 +9,7 @@ PLACEHOLDER
 import numpy as np
 import shap
 import yaml
-# import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from os import makedirs, path, environ
@@ -38,7 +38,7 @@ def main():
 	with open(config_path, 'r') as f:
 		config = yaml.safe_load(f)
 
-	# FEATURES = config['FEATURES']
+	FEATURES = config['FEATURES']
 	MODEL_DIR = path.join(environ['PROJECT_ROOT'], config['MODEL_DIR'], f"model_{model_index}/set_{set_index}")
 	TRAIN_DATA_DIR = path.join(environ['PROJECT_ROOT'], config['DATA_DIR'], f"train/model_{model_index}")
 
@@ -71,42 +71,28 @@ def main():
 		explainer = shap.GradientExplainer(model, background)
 		shap_values = explainer(X_test)
 
-	# Gráfico de importancia bruta
+	# Crear DataFrame
+	importance_df = pd.DataFrame({
+		'Feature': FEATURES[:-1],
+		'SHAP_values': shap_values
+	})
+
+	print(importance_df.head)
+	print(importance_df.shape)
+
 	plt.figure(figsize=(12, 8))
-	shap.plots.bar(shap_values)
+	plt.barh(
+		importance_df['Feature'], importance_df['SHAP_mean'],
+		color=np.where(importance_df['SHAP_values'] > 0, 'skyblue', 'salmon')
+	)
 	plt.xlabel('Valor SHAP promedio', fontsize=12)
 	plt.ylabel('Feature', fontsize=12)
 	plt.title('Impacto de Features en la Predicción (SHAP Values)', fontsize=14)
+	plt.axvline(0, color='black', linestyle='--', linewidth=0.5)
+	plt.gca().invert_yaxis()
 	plt.tight_layout()
 	plt.savefig(path.join(output_path_base, f'{model_index}_{set_index}_fold_{train_fold}_shap_values.pdf'))
 	plt.close()
-
-	# # Convertir la lista de arrays en un array 3D (folds, samples, features)
-	# shap_values_array = np.array(shap_values_per_fold)
-
-	# # Calcular la media de SHAP para cada feature (conservando el signo)
-	# mean_shap = np.mean(shap_values_array, axis=(0, 1)).flatten()
-
-	# # Crear DataFrame
-	# importance_df = pd.DataFrame({
-	# 	'Feature': FEATURES[:-1],
-	# 	'SHAP_mean': mean_shap
-	# })
-
-	# plt.figure(figsize=(12, 8))
-	# plt.barh(
-	# 	importance_df['Feature'], importance_df['SHAP_mean'],
-	# 	color=np.where(importance_df['SHAP_mean'] > 0, 'skyblue', 'salmon')
-	# )
-	# plt.xlabel('Valor SHAP promedio', fontsize=12)
-	# plt.ylabel('Feature', fontsize=12)
-	# plt.title('Impacto de Features en la Predicción (SHAP Values)', fontsize=14)
-	# plt.axvline(0, color='black', linestyle='--', linewidth=0.5)
-	# plt.gca().invert_yaxis()
-	# plt.tight_layout()
-	# plot_path_signed = f"{output_path_base}/feature_impact_shap.pdf"
-	# plt.savefig(plot_path_signed)
-	# plt.close()
 
 
 if __name__ == "__main__":

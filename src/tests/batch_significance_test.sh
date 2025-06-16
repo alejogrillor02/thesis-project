2b4c1ab2070558671c933abc397013abde57735d
alejogrillor02 <alejo.grillor02@protonmail.com>
2025-06-16 17:41:16 -0400

    AAA

--- src/tests/batch_significance_test.sh
+++ src/tests/batch_significance_test.sh
@@ -26,6 +26,12 @@ echo
 echo "Doing statistical Tests on G and M with G test set"
 ./significance_test.py "${OUTPUT_DIR}/model_903/set_G/predictions/903_G_fold_metrics.csv" "${OUTPUT_DIR}/model_903/set_M/cross_eval/set_G/903_M_with_G_fold_metrics.csv"
 echo
+echo "Doing statistical Tests on F and G with F test set"
+./significance_test.py "${OUTPUT_DIR}/model_903/set_F/predictions/903_F_fold_metrics.csv" "${OUTPUT_DIR}/model_903/set_G/cross_eval/set_F/903_G_with_F_fold_metrics.csv"
+echo
+echo "Doing statistical Tests on M and G with M test set"
+./significance_test.py "${OUTPUT_DIR}/model_903/set_M/predictions/903_M_fold_metrics.csv" "${OUTPUT_DIR}/model_903/set_G/cross_eval/set_M/903_G_with_M_fold_metrics.csv"
+echo
 echo "Doing statistical Tests on F and M with F test set"
 ./significance_test.py "${OUTPUT_DIR}/model_903/set_F/predictions/903_F_fold_metrics.csv" "${OUTPUT_DIR}/model_903/set_M/cross_eval/set_F/903_M_with_F_fold_metrics.csv"
 echo
